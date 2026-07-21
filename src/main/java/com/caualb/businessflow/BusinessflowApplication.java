1bb4a72bae8da081ff4271b331d1f8ef9e376cc7
Cauã Albuquerque <caua.albuquerque62@aluno.ifce.edu.br>
2026-07-21 12:11:55 -0300

    chore: configuração inicial do projeto

---
+++ src/main/java/com/caualb/businessflow/BusinessflowApplication.java
@@ -0,0 +1,13 @@
+package com.caualb.businessflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caualb/businessflow/BusinessflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caualb.businessflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
